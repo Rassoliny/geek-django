ef11ef6f904dd0b46b21513e88433494eba0b59a
Rassoliny <dimo120@yandex.ru>
2018-08-16 17:37:49 +0300

    First page works without design

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp.views import mysite
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mysite', mysite),
 ]
