72ce6fa797577f79c658db7e30b15f23109bf7aa
Rassoliny <dimo120@yandex.ru>
2018-08-20 23:52:05 +0300

    Changes product title in database

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -7,3 +7,8 @@ class Product(models.Model):
     title = models.CharField(max_length=150)
     image = models.ImageField(blank=True, null=True)
     cost = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+    def __str__(self):
+
+        return self.title
