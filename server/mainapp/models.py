cc01f96f3fe1c77f94665ca80123ff76b6c6af40
Rassoliny <dimo120@yandex.ru>
2018-08-23 23:46:41 +0300

    All content work with Product models

--- server/mainapp/models.py
+++ server/mainapp/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     title = models.CharField(max_length=150)
     image = models.ImageField(blank=True, null=True)
     cost = models.DecimalField(max_digits=7, decimal_places=2)
-
+    short_description = models.TextField(blank=True, max_length=300)
+    description = models.TextField(blank=True, max_length=500)
 
     def __str__(self):
 
