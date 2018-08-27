c21996af9718486373785cc65c1cdaec8945d636
Rassoliny <dimo120@yandex.ru>
2018-08-27 23:39:55 +0300

    fill_db ready to post in database

--- server/mainapp/management/commands/fill_db.py
+++ server/mainapp/management/commands/fill_db.py
@@ -5,6 +5,29 @@ from authapp.models import ShopUser
 import json, os
 
 
+JSON_PATH = 'mainapp/json'
+
+def loadFromJSON(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+        return json.load(infile)
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        super_user = ShoUser.objects.create_superuser('adm', 'adm@kovashop.local', 'qwe123', age = 25)
+        categories = loadFromJSON('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            new_category = ProductCategory(**category)
+            new_category.save()
+
+        products = loadFromJSON('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product["category"]
+            _category = ProductCategory.objects.get(name=category_name)
+            product['category'] = _category
+            new_product = Product(**product)
+            new_product.save()
+
+        # super_user = ShoUser.objects.create_superuser('adm', 'adm@kovashop.local', 'qwe123', age = 25)
