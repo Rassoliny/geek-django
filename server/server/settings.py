a204793ad6a43f89fd5b63a796923df336489b5a
Rassoliny <dimo120@yandex.ru>
2018-08-27 23:11:33 +0300

    Made authapp model

--- server/server/settings.py
+++ server/server/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mainapp',
+    'authapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,6 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+AUTH_USER_MODEL = 'authapp.ShopUser'
 #
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
