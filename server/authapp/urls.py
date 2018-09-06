733e32caeee45b6a0a4663d4c50754331abc0073
Rassoliny <dimo120@yandex.ru>
2018-09-06 07:08:55 +0300

    Authapp is working. Changes in authapp: views, urls, forms, templates; mainapp: menu, server: urls

---
+++ server/authapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+from . import views as authapp
+
+app_name = 'auth'
+
+
+urlpatterns = [
+    re_path(r'^login', authapp.login, name='login'),
+    re_path(r'^logout', authapp.logout, name='logout'),
+    re_path(r'^edit', authapp.edit, name='edit'),
+
+]
--- server/server/urls.py
+++ server/server/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import re_path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -31,5 +31,6 @@ urlpatterns = [
     re_path(r'^mirrow', mainapp.product_mirrow, name='mirrow'),
     re_path(r'^box', mainapp.product_box, name='box'),
     re_path(r'(?P<pk>\d+)', mainapp.product_detail, name='detail'),
+    re_path(r'^auth', include('authapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
