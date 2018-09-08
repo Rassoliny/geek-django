52e8e5e17e889683aa075b3a8ad2357fbeb0fbb6
Rassoliny <dimo120@yandex.ru>
2018-09-08 09:52:53 +0300

    Made products in mainapp.views (catalog will work with categories)

--- server/mainapp/views.py
+++ server/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 # Create your views here.
 from django.shortcuts import HttpResponse
@@ -26,3 +26,37 @@ def product_box(request):
 def product_detail(request, pk):
     instance = models.Product.objects.get(id=pk)
     return render(request, 'products/detail.html', {'instance': instance})
+
+def products(request, pk):
+    print(pk)
+
+    title = 'Продукты'
+    links_menu = models.ProductCategory.objects.all()
+
+    if pk:
+        if pk == '0':
+            category = {'name': 'все'}
+            products = models.Product.objects.all().order_by('cost')
+        else:
+            category = get_object_or_404(models.ProductCategory, pk=pk)
+            products = models.Product.objects.filter(category__pk=pk).order_by('cost')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = models.Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products,
+
+    }
+
+    return render(request, 'mainapp/products.html', content)
