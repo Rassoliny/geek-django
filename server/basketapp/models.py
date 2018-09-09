900317d48ae13a9f279a626789f48562af485f06
Rassoliny <dimo120@yandex.ru>
2018-09-09 08:19:08 +0300

    Functions in basketapp/models.py

--- server/basketapp/models.py
+++ server/basketapp/models.py
@@ -8,3 +8,22 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='Количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='Время', auto_now_add=True)
+
+
+    def _get_product_cost(self):
+        return self.product.cost * self.quantity
+
+
+    def _get_total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _totalquantity
+
+    total_quantity = property(_get_total_quantity)
+
+    def _get_total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        return _totalcost
+
+    total_cost = property(_get_total_cost)
