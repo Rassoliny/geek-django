5741005dc98e2690187e8067416bee6e1be67151
Rassoliny <dimo120@yandex.ru>
2018-09-09 07:04:25 +0300

    Made basket.urls

---
+++ server/basketapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from . import views as baskethapp
+
+app_name = 'basket'
+
+
+urlpatterns = [
+    re_path(r'^$', baskethapp.basket, name='view'),
+    re_path(r'^add/(?P<pk>\d+)', baskethapp.basket_add, name='add'),
+    re_path(r'^remove/(?P<pk>\d+)', baskethapp.basket_remove, name='remove'),
+
+]
+
