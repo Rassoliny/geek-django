254a554db283d0c932fefafbc88dd747a50a1f16
Rassoliny <dimo120@yandex.ru>
2018-09-10 06:56:27 +0300

    Changes for basket and hot_products in mainapp.views

--- server/mainapp/views.py
+++ server/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
 from basketapp.models import Basket
+import random
 # Create your views here.
 from django.shortcuts import HttpResponse
 
@@ -11,30 +12,52 @@ def main(request):
     content = {'products': products}
     return render(request, 'mainapp/index.html', content)
 
+
 def catalog(request):
     query = models.Product.objects.all()
     return render(request, 'mainapp/catalog.html', {'results': query})
 
+
 def contacts(request):
     return render(request, 'mainapp/contacts.html', {})
+
 
 def product_mirrow(request):
     return render(request, 'products/mirrow.html', {})
 
+
 def product_box(request):
     return render(request, 'products/box.html', {})
+
 
 def product_detail(request, pk):
     instance = models.Product.objects.get(id=pk)
     return render(request, 'products/detail.html', {'instance': instance})
 
-def products(request, pk):
-    print(pk)
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_products():
+    products = models.Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = models.Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
+
+def products(request, pk=None):
     title = 'Продукты'
     links_menu = models.ProductCategory.objects.all()
+    basket = get_basket(request.user)
 
-    basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
 
@@ -56,12 +79,16 @@ def products(request, pk):
 
         return render(request, 'mainapp/products_list.html', content)
 
-    same_products = models.Product.objects.all()[3:5]
+    hot_product = get_hot_products()
+    same_products = get_same_products(hot_product)
+
 
     content = {
         'title': title,
         'links_menu': links_menu,
+        'hot_product': hot_product,
         'same_products': same_products,
+        'basket': basket,
 
     }
 
