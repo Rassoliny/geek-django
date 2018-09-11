c2e4bebab8f6b200e3279d33e2ecdf7d7eb1e487
Rassoliny <dimo120@yandex.ru>
2018-09-11 06:17:59 +0300

    Made controllerls in basket.views

--- server/basketapp/views.py
+++ server/basketapp/views.py
@@ -4,7 +4,12 @@ from mainapp.models import Product
 # Create your views here.
 
 def basket(request):
-    content = {}
+    title = 'Корзина'
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+    content = {
+        'title': title,
+        'basket_items': basket_items,
+    }
     return render(request, 'basketapp/basket.html', content)
 
 def basket_add(request, pk):
@@ -21,6 +26,8 @@ def basket_add(request, pk):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-def basket_remove(request):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
