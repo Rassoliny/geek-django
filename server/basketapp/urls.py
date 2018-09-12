f18f4e67e4055762b8e4964bc177b2ee5619baab
Rassoliny <dimo120@yandex.ru>
2018-09-12 07:09:19 +0300

    Made def basket_edit

--- server/basketapp/urls.py
+++ server/basketapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     re_path(r'^$', baskethapp.basket, name='view'),
     re_path(r'^add/(?P<pk>\d+)', baskethapp.basket_add, name='add'),
     re_path(r'^remove/(?P<pk>\d+)', baskethapp.basket_remove, name='remove'),
+    re_path(r'^edit/(?P<pk>\d+)/(?P<quantity>\d+)', baskethapp.basket_edit, name='edit'),
 
 ]
 
