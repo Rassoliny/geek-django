8c030f5b2f17c814a61adcf3b5f757ce3653db17
Rassoliny <dimo120@yandex.ru>
2018-09-12 21:59:58 +0300

    Changes in register function and new send_verify method

--- server/authapp/views.py
+++ server/authapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import ShopUser
 # Create your views here.
 
 
@@ -57,13 +60,34 @@ def register(request):
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
 
         if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
+            user = register_form.save()
+            if send_verify_mail(user):
+                print('Confirmation was send')
+                return HttpResponseRedirect(reverse('auth:login'))
+            else:
+                print('Fail send confirmation')
+                return HttpResponseRedirect(reverse('auth:login'))
 
-    else:
-        register_form = ShopUserRegisterForm()
+        else:
+            register_form = ShopUserRegisterForm()
+            content = {'title': title, 'register_form': register_form}
+            return render(request, 'authapp/register.html', content)
 
-    content = {'title': title, 'register_form': register_form}
 
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
 
-    return render(request, 'authapp/register.html', content)
+    title = f'Подтверждение учетной записи {user.username}'
+    message = f'Для подтверждения учетной записи {user.username}на портале {settings.DOMAIN_NAME} ' \
+              f'перейдите по ссылке:\n' \
+              f'{settings.DOMAIN_NAME}{verify_link}'
+
+    print(f'from: {settings.EMAIL_HOST_USER}, to: {user.email}')
+    return send_mail(
+            title,
+            message,
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+        )
+
