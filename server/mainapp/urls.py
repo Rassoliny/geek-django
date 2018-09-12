3307d18b62f36916e1dd174aed00e43b5a7bb11f
Rassoliny <dimo120@yandex.ru>
2018-09-12 06:45:01 +0300

    Add url name 'product' in mainapp.urls

--- server/mainapp/urls.py
+++ server/mainapp/urls.py
@@ -5,7 +5,8 @@ app_name = 'products'
 
 urlpatterns = [
     re_path(r'^$', mainapp.main, name='main'),
-    re_path(r'(?P<pk>\d+)', mainapp.products, name='category'),
+    re_path(r'products/(?P<pk>\d+)', mainapp.products, name='category'),
+    re_path(r'(?P<pk>\d+)', mainapp.product_detail, name='product'),
 
 ]
 
