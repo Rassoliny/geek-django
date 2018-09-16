08e32a9046dde042a032dad8a3d347a692ceaad1
Rassoliny <dimo120@yandex.ru>
2018-09-16 19:19:11 +0300

    Changes in edit method

--- server/authapp/views.py
+++ server/authapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
-from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.urls import reverse
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db import transaction
 from .models import ShopUser
+from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
+
 # Create your views here.
 
 
@@ -36,25 +38,25 @@ def logout(request):
     return HttpResponseRedirect(reverse('main'))
 
 
-def edit(request):
-    title = 'Редактирование'
-
-
-    if request.method == 'GET':
-        edit_form = ShopUserEditForm(request.GET, request.FILES, instance=request.user)
-
-    if request.method == 'POST':
-        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(reverse('auth:edit'))
-        else:
-            edit_form = ShopUserEditForm(instance=request.user)
-
-    content = {'title': title, 'edit_form': edit_form}
-
-    return render(request, 'authapp/edit.html', content)
+# def edit(request):
+#     title = 'Редактирование'
+#
+#
+#     if request.method == 'GET':
+#         edit_form = ShopUserEditForm(request.GET, request.FILES, instance=request.user)
+#
+#     if request.method == 'POST':
+#         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+#
+#         if edit_form.is_valid():
+#             edit_form.save()
+#             return HttpResponseRedirect(reverse('auth:edit'))
+#         else:
+#             edit_form = ShopUserEditForm(instance=request.user)
+#
+#     content = {'title': title, 'edit_form': edit_form}
+#
+#     return render(request, 'authapp/edit.html', content)
 
 
 def register(request):
@@ -115,3 +117,26 @@ def verify(request, email, activation_key):
         print(f'error activation user : {e.args}')
 
     return HttpResponseRedirect(reverse('main'))
+
+
+@transaction.atomic
+def edit(request):
+    title = 'Редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        profile_form = ShopUserProfileEditForm(request.POST, instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(request('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+        profile_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
+
+    content = {
+        'title': title,
+        'edit_form': edit_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
