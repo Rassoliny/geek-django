93ec8546fa1f14999fd7bab024126d26d9f1f9b2
Rassoliny <dimo120@yandex.ru>
2018-09-16 19:11:51 +0300

    ShopUserProfileEditForm

--- server/authapp/forms.py
+++ server/authapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 import random, hashlib
 
 
@@ -68,3 +68,14 @@ class ShopUserEditForm(UserChangeForm):
             raise forms.ValidationError("Вы слишком молоды!")
 
         return data
+
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'aboutMe', 'gender')
+
+        def __init__(self, *args, **kwargs):
+            super(ShopUserProfile, self).__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
