b526b3087d2d0c2bfb219b5df7fd7bd72753c593
Rassoliny <dimo120@yandex.ru>
2018-09-16 14:12:55 +0300

    get_activation_ket_expires

--- server/authapp/models.py
+++ server/authapp/models.py
@@ -7,6 +7,9 @@ from django.utils.timezone import now
 from datetime import timedelta
 
 
+def get_activation_key_expires():
+    return now() + timedelta(hours=48)
+
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст')
@@ -14,7 +17,7 @@ class ShopUser(AbstractUser):
     activation_key = models.CharField(verbose_name='ключ подтверждения', max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(
         verbose_name='актуальность ключа',
-        default=(now() + timedelta(hours=48)))
+        default=get_activation_key_expires
 
     def is_activation_key_expired(self):
         if now() <= self.activation_key_expires:
